6df7ecb68d5779c5b02931c6e9d5ea3a5d62f37f
asadalibest1 <asadalibest1@gmail.com>
2020-08-14 22:48:03 -0700

    Updates

---
+++ precache-manifest.111b2996f91d154e38f2ad8b8b142906.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01599e272dca3f2f569dbe5ff914f1e7",
+    "url": "/RedQueenReactApp.github.io/index.html"
+  },
+  {
+    "revision": "9649960a1361269f300b",
+    "url": "/RedQueenReactApp.github.io/static/css/main.3ea922da.chunk.css"
+  },
+  {
+    "revision": "c25b01079194f396896e",
+    "url": "/RedQueenReactApp.github.io/static/js/2.211f2996.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RedQueenReactApp.github.io/static/js/2.211f2996.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9649960a1361269f300b",
+    "url": "/RedQueenReactApp.github.io/static/js/main.55032b73.chunk.js"
+  },
+  {
+    "revision": "b03bd6a0c0c44aa2ba1f",
+    "url": "/RedQueenReactApp.github.io/static/js/runtime-main.ea8ee6ad.js"
+  }
+]);
